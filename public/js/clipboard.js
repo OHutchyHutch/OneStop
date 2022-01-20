f00e43c78e2b6e4f06a09a19cec7c5dce8c62cb6
OHutchyHutch <ohutchins1@student.gsu.edu>
2022-01-19 19:05:48 -0500

    Fixed Copy and Paste

--- public/js/clipboard.js
+++ public/js/clipboard.js
@@ -1,17 +1,31 @@
-async function copyToClipboard(value) {
-    var range = document.createRange();
-    range.selectNode(document.getElementById(value));
-    window.getSelection().removeAllRanges(); // clear current selection
-    window.getSelection().addRange(range); // to select text
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function copyToClipboard(value, element) {
+    // Create a "hidden" input
+    var aux = document.createElement("input");
+
+    // Assign it the value of the specified element
+    aux.setAttribute("value", value);
+
+    // Append it to the body
+    document.body.appendChild(aux);
+
+    // Highlight its content
+    aux.select();
+
+    // Copy the highlighted text
     document.execCommand("copy");
-    window.getSelection().removeAllRanges();// to deselect
-    var el = document.querySelector(`#${value} #clipboard`);
+
+    // Remove it from the body
+    document.body.removeChild(aux);
+    var el = document.querySelector(`#${element} #clipboard`);
     el.classList.remove('bi-clipboard');
     el.classList.add('bi-clipboard-check');
     await sleep(2000);
     el.classList.remove('bi-clipboard-check');
     el.classList.add('bi-clipboard');
-}
-function sleep(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+
 }
